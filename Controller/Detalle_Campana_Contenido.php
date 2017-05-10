0c0075b16c0c5106363567726ef3a09c40d2affb
Pignatar0md <pignataromd@gmail.com>
2017-05-10 14:10:40 -0300

    get agent test

--- Controller/Detalle_Campana_Contenido.php
+++ Controller/Detalle_Campana_Contenido.php
@@ -3,10 +3,10 @@ session_start();
 include $_SERVER['DOCUMENT_ROOT'] . '/Omnisup/config.php';
 include controllers . '/Campana.php';
 include controllers . '/Agente.php';
-include entities . '/QueueMember.php';
+
 include helpers . '/time_helper.php';
 
-$QM = new QueueMember();
+
 $Controller_Campana = new Campana();
 //$Controller_Agente = new Agente();
 $jsonString = '';
