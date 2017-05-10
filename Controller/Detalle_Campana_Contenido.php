b0b2781ea2403008871fa3df580a4976c21dcdea
Pignatar0md <pignataromd@gmail.com>
2017-05-10 14:35:27 -0300

    see cmd result

--- Controller/Detalle_Campana_Contenido.php
+++ Controller/Detalle_Campana_Contenido.php
@@ -15,7 +15,7 @@ if ($_GET['nomcamp']) {
 
     $resul = $Controller_Campana->traerCampanaDet($_GET['nomcamp']);
     $jsonString .= '[';
-    var_dump($resul);
+    echo $resul;
     $jsonString = substr($jsonString, 0, -1);
     //echo $jsonString . ']';
 }
