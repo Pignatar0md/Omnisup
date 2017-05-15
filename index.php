743794306ad5e8628819f2c881692b6765615c6f
Pignatar0md <pignataromd@gmail.com>
2017-05-15 10:33:18 -0300

    new layout

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="static/Css/jquery.dataTables.min.css"/>
         <link rel="stylesheet" type="text/css" href="static/Css/phone.css"/>
+        <link rel="stylesheet" type="text/css" href="static/Css/main.css"/>
         <title>OmniLeads</title>
     </head>
     <body>
