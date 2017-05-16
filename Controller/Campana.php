cb6b1b4d95b081d3cab84600cd449fef324374ab
Pignatar0md <pignataromd@gmail.com>
2017-05-16 13:58:42 -0300

    add space on traerCampanaDet function

--- Controller/Campana.php
+++ Controller/Campana.php
@@ -48,28 +48,28 @@ class Campana {
 
     function traerCampanaDet($nomCamp) {
         $campana = $this->Campana_Model->getCampaign($nomCamp);
-	$rawArrayData = array();
-	$campana = explode(PHP_EOL, $campana);
-	foreach($campana as $clave => $valor) {
-	    $Qm = new QueueMember();
-	    $valor = explode("(", $valor);
-	    $name = str_replace(")","",$valor[0]);
-	    if($name) {
-	        $Qm->setName($name);
-	    }
-	    $val = explode(" ", $valor[1]);
-	    $exten = str_replace("SIP/","",$val[0]);
-	    if($exten) {
-	        $Qm->setExten($exten);
-	    }
-	    $val = str_replace("(","",$valor[2]);
-	    $val = str_replace(")","",$valor[2]);
-	    $status = $val;
-	    if($status) {
-	        $Qm->setStatus($status);
-	    }
-	    $rawArrayData[] = $Qm;
-	}
+      	$rawArrayData = array();
+      	$campana = explode(PHP_EOL, $campana);
+      	foreach($campana as $clave => $valor) {
+      	    $Qm = new QueueMember();
+      	    $valor = explode("(", $valor);
+      	    $name = str_replace(")","",$valor[0]);
+      	    if($name) {
+      	        $Qm->setName($name);
+      	    }
+      	    $val = explode(" ", $valor[1]);
+      	    $exten = str_replace("SIP/","",$val[0]);
+      	    if($exten) {
+      	        $Qm->setExten($exten);
+      	    }
+      	    $val = str_replace("(","",$valor[2]);
+      	    $val = str_replace(")","",$valor[2]);
+      	    $status = $val;
+      	    if($status) {
+      	        $Qm->setStatus($status);
+      	    }
+      	    $rawArrayData[] = $Qm;
+      	}
         return $rawArrayData;
     }
 
