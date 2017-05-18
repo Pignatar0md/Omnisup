0d30419bf64d98679593fcf547695f5dc47ad73a
Pignatar0md <pignataromd@gmail.com>
2017-05-18 17:10:47 -0300

    sad

--- Model/Campana_Model.php
+++ Model/Campana_Model.php
@@ -56,8 +56,8 @@ class Campana_Model {
     }
 
     function getConnectedCalls($CampName) {
-      $sql = "select count(*) from ominicontacto_app_queuelog where EXTRACT(DAY FROM time) = 17 and " .
-      "EXTRACT(MONTH FROM time) = 05 and EXTRACT(YEAR FROM time) = 2017 and event like 'ENTERQUEUE' and queuename like 'multinum-3'";
+      $sql = "select count(*) from ominicontacto_app_queuelog where EXTRACT(DAY FROM time) = :dia and " .
+      "EXTRACT(MONTH FROM time) = :mes and EXTRACT(YEAR FROM time) = :ano and event like 'ENTERQUEUE' and queuename like :campname";
       $day = date("d");
       $month = date("m");
       $year = date("Y");
