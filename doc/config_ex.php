1164b368bd1473a110b6b34cc81b35fefd3bd62e
pignataromd@gmail.com <pignataromd@gmail.com>
2017-11-16 15:33:13 -0300

    cambia PG_HOST

--- doc/config_ex.php
+++ doc/config_ex.php
@@ -6,7 +6,7 @@ define('AMI_PASWORD','fop222');
 define('AMI_HOST','127.0.0.1');
 define('PG_USER', 'kamailio');
 define('PG_PASSWORD', 'kamailiorw');
-define('PG_HOST', '127.0.0.1');
+define('PG_HOST', $_SERVER['SERVER_ADDR'].':10443');
 define('WD_API_USER', 'demoadmin');
 define('WD_API_PASS', 'demo');
 define("entities", $_SERVER['DOCUMENT_ROOT'].'/Omnisup/entities');
